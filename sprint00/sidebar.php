33907521d0189df8ca617dca729916ecf9c69449
Twan Natter <t.natter@nubix.nl>
2018-09-05 14:10:53 +0200

    Commit

--- sprint00/sidebar.php
+++ sprint00/sidebar.php
@@ -9,22 +9,14 @@ echo
     <div class="dropdown-container">
         <a href="../HTML/thisisme.php">weekdagen.html</a>
         <a href="../HTML/nlvlag.html">nlvlag.html</a>
-        <a href="../php/compare.php">compare.php</a>
-        <a href="../php/operators.php">operators.php</a>
-        <a href="../php/moreoperators.php">moreoperators.php</a>
-        <a href="../php/dobbel1.php">dobbel1.php</a>
-        <a href="../php/onzegroep.php">onzegroep.php</a>
-        <a href="../php/eredivisie1.php">eredivisie1.php</a>
-        <a href="../php/kaartspel1.php">kaartspel1.php</a>
     </div>
     
-        <button class="dropdown-btn">Backend
+        <button class="dropdown-btn">PHP
         <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-container">
-        <a href="../Backend/lab01.php">Lab 01</a>
-        <a href="../Backend/lab02.php">Lab 02</a>
-        <a href="../Backend/lab03.php">Lab 03</a>
+        <a href="../PHP/weekdagen.php">weekdagen.php</a>
+        <a href="../PHP/nlvlag.php">nlvlag.php</a>
     </div>
 <script>
     /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
