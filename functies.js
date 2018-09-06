3688cf4f891ec291f907dfc501036b9f6c67692b
Twan Natter <t.natter@nubix.nl>
2018-09-06 08:57:00 +0200

    Opgeschoond

--- functies.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    var code = prompt("Please enter your code", "Uw Code");
-    if (code != null) {
-        document.getElementById("demo").innerHTML =
-            "Uw ingevoerde code is " + code + "";
-    }
-}
